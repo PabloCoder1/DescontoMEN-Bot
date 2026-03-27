e563cdc13884e07e0149649e0550c3e2157b1eb3
PABLO LIMA DOS SANTOS <ps217348@alunos.unisanta.br>
2026-03-26 22:58:08 -0300

    v39.0: Final stable release with project cleanup and ironclad price protection

--- encurtador.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def encurtar_link(link_longo):
-    try:
-        url_api = f"http://tinyurl.com/api-create.php?url={link_longo}"
-        res = requests.get(url_api, timeout=10)
-        return res.text if res.status_code == 200 else link_longo
-    except:
-        return link_longo
